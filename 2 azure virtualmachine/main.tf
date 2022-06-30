ae28af99c5616c502398cc7053fc98a196b659b3
Your Name <you@example.com>
2022-06-30 01:06:36 +0100

    yu

--- 2 azure virtualmachine/main.tf
+++ 2 azure virtualmachine/main.tf
@@ -151,3 +151,19 @@ resource "azurerm_windows_virtual_machine" "appvm" {
     azurerm_resource_group.appgrp
   ]
 }
+
+resource "azurerm_managed_disk" "appdisk" {
+  name                 = "appdisk"
+  location             = local.location
+  resource_group_name  = local.resoure_group_name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1"
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "appdiskattach" {
+  managed_disk_id    = azurerm_managed_disk.appdisk.id
+  virtual_machine_id = azurerm_windows_virtual_machine.appvm.id
+  lun                = "10"
+  caching            = "ReadWrite"
+}
